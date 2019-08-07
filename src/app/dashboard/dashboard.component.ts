4d578e43236b752bd89dada6f80f73c43e7ab7b0
Marina Rey <marinarey2@hotmail.com>
2019-08-06 22:52:13 -0300

    melhorias no dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 import { Tag } from "../recipe";
+import { Recipe } from '../recipe';
 import { RecipeService } from '../recipe.service';
 import { Router } from '@angular/router';
 
@@ -12,8 +13,11 @@ import { Router } from '@angular/router';
 export class DashboardComponent implements OnInit {
 
   tags: Tag[];
+  recipes: Recipe[];
   numberOfRecipes: number;
   numberOfUsers: number;
+  userLevel: number;
+  isLoggedIn: boolean;
 
   constructor(private recipeService: RecipeService, private router: Router) { }
 
@@ -22,12 +26,33 @@ export class DashboardComponent implements OnInit {
     this.recipeService.getTags()
       .subscribe(tags => this.tags = tags);
 
+    this.recipeService.getRecipes()
+      .subscribe(recipes => this.recipes = recipes);
+
     this.recipeService.getNumberOfRecipes().subscribe(num => this.numberOfRecipes = num);
     this.recipeService.getNumberOfUsers().subscribe(num => this.numberOfUsers = num);
 
+    this.isLoggedIn = this.recipeService.isLoggedIn;
+    this.userLevel = this.recipeService.getUserLevel();
+
+    /*
     var graphData;
     this.recipeService.organizeChartData().subscribe(data => graphData = data);
-    this.createGraph(graphData);
+    this.createGraph(graphData);*/
+
+   // $('.next').click(function () { $('.carousel').carousel('next'); return false; });
+   // $('.prev').click(function () { $('.carousel').carousel('prev'); return false; });
+
+  }
+
+  next()
+  {
+    $('.carousel').carousel('next'); return false;
+  }
+
+  prev()
+  {
+    $('.carousel').carousel('prev'); return false;
   }
 
   goToNewRecipePage() {
@@ -37,6 +62,13 @@ export class DashboardComponent implements OnInit {
     else {
       window.alert("Você não possui autorização para adicionar receitas!");
     }
+  }
+  recipeCarouselCounter(i: number) {
+    return new Array(Math.ceil(i/3));
+}
+
+  getImageSrc(index, recipe) {
+    return "../../../../backend/uploads/" + recipe.photos[index];
   }
 
   createGraph(data): void {
